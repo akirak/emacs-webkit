796e983d69fe17e48016d543c34e731d4cbd67fd
Akira Komamura <akira.komamura@gmail.com>
2021-10-11 23:23:03 +0900

    Drop shell.nix which is unnecessary when you have a flake

--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    buildInputs = with pkgs; [
-      gcc pkg-config gtk3 webkitgtk glib-networking wrapGAppsHook
-      gdb
-    ];
-
-    GIO_EXTRA_MODULES = "${pkgs.glib-networking}/lib/gio/modules:${pkgs.dconf.lib}/lib/gio/modules";
-}
